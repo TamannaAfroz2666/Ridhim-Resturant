b8719978a025e6f91a54bd59b22940ec6f2696da
TamannaAfroz2666 <afroztamanna66@gmail.com>
2023-10-08 14:11:04 +0600

    carosel add

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,12 +1,24 @@
 import React from 'react';
+import Button from 'react-bootstrap/Button';
+
+// import ExampleCarouselImage from './components/ExampleCarouselImage';
+
+
 
 const Home = () => {
-    return (
-        <div>
-            
-            
-        </div>
-    );
+
+  return (
+    <div className='home'>
+      <Button variant="primary">Primary</Button>
+      <br />
+     
+
+
+
+
+
+    </div>
+  );
 };
 
 export default Home;
